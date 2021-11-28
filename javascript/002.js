d28f3d21b4eb6c6ae93fe121fafb4794455adab9
sohyeonAn <ansh4393@gmail.com>
2021-11-29 08:57:27 +0900

    11월 26일 수업, 과제>커스텀 셀렉트박스 모범답안

---
+++ javascript/002.js
@@ -0,0 +1,23 @@
+// var name = '사용자';
+// var age = 20;
+
+// document.getElementById('title').innerHTML = 'Welcome to JS100제';
+// document.write(20 + 20);
+// window.alert(name);
+// console.log(age);
+
+
+var myVal1 = "dasdsdsdsds";
+var myVal2 = 10192192919;
+var myVal3 = [1,2,3,4,5];
+var myVal4 = {val1: 1, val2: 2};
+var myVal5 = function () {
+  return false;
+}
+
+var myVal6=[1,2,3], myVal7 =[4,5,6];
+var myVal8 = "조은선생님";
+var MyVal8 = "조운선생님";
+
+window.alert(myVal8);
+window.alert(MyVal8);
