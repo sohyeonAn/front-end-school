8604666a2f1814aa596955f4d3ca50ae3a1223c5
sohyeonAn <ansh4393@gmail.com>
2021-12-08 19:02:19 +0900

    12월 8일 수업

---
+++ javascript/callback함수.js
@@ -0,0 +1,36 @@
+// 정리 전
+
+loadScript('1.js', function(error, script) {
+
+  if (error) {
+    handleError(error);
+  } else {
+    // ...
+    loadScript('2.js', function(error, script) {
+      if (error) {
+        handleError(error);
+      } else {
+        // ...
+        loadScript('3.js', function(error, script) {
+          if (error) {
+            handleError(error);
+          } else {
+            // 모든 스크립트가 로딩된 후, 실행 흐름이 이어집니다. (*)
+          }
+        });
+
+      }
+    })
+  }
+});
+
+// 정리 후
+loadScript("/article/promise-chaining/one.js")
+.then(script => loadScript("/article/promise-chaining/two.js"))
+.then(script => loadScript("/article/promise-chaining/three.js"))
+.then(script => {
+  // 스크립트를 정상적으로 불러왔기 때문에 스크립트 내의 함수를 호출할 수 있습니다.
+  one();
+  two();
+  three();
+});
