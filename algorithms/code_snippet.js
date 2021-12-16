f8a8492bcf257555b1f21d9a8552ff8e482c17e6
sohyeonAn <ansh4393@gmail.com>
2021-12-16 15:51:57 +0900

    12월 16일 수업 - 알고리즘(트리, 카카오 18년도)

--- algorithms/code_snippet.js
+++ algorithms/code_snippet.js
@@ -306,26 +306,285 @@
 
 
 // 퀵 정렬
-function quickSort(input){
-  let inputLen = input.length;
+// function quickSort(input){
+//   let inputLen = input.length;
 
-  if(inputLen <= 1){
-    return input;
-  }
+//   if(inputLen <= 1){
+//     return input;
+//   }
 
-  const pivot = input.shift(); // 기준값
-  const grp1 = [];
-  const grp2 = [];
+//   const pivot = input.shift(); // 기준값
+//   const grp1 = [];
+//   const grp2 = [];
 
-  for(let i in input) {
-    if(input[i] < pivot){
-      grp1.push(input[i]);
-    } else {
-      grp2.push(input[i]);
-    }
-  }
+//   for(let i in input) {
+//     if(input[i] < pivot){
+//       grp1.push(input[i]);
+//     } else {
+//       grp2.push(input[i]);
+//     }
+//   }
 
-  return quickSort(grp1).concat(pivot, quickSort(grp2));
+//   return quickSort(grp1).concat(pivot, quickSort(grp2));
+// }
+// let input = [199, 22, 33, 12, 32, 64, 72, 222, 233];
+// console.log(quickSort(input));
+
+// 트리
+// 너비우선탐색(BFS) - 큐 (push, shift)
+// 깊이우선탐색(DFS) - 스택 (push, pop)
+// current에서 visitPath로 가는 순간 자신의 자식 노드가 스택/큐에 추가
+
+// 트리의 기본 형태
+// const tree = {
+//   root: {
+//       value: 5,
+//       left: {
+//           value: 3,
+//           left: {
+//               value: 1,
+//               left : null,
+//               right: null
+//           },
+//           right : {
+//               value: 4,
+//               left : null,
+//               right: null
+//           }
+//       },
+//       right: {
+//           value: 8,
+//           left: {
+//               value: 6,
+//               left : null,
+//               right: null
+//           },
+//           right : {
+//               value: 9,
+//               left : null,
+//               right: null
+//           }
+//       }
+//   }
+// };
+// // 트리를 object로 만들 수 있었는데 굳이 클래스로 만드는 이유는?
+// // 구조적 확장 가능성.
+// // 객체 지향 프로그래밍의 철학과 맞기 때문
+// class Node {
+//   constructor(value){
+//     this.value = value;
+//     this.child = []; // 이렇게 하면 이진트리말고 그 이상도 만들 수 있다.
+//   }
+
+// }
+
+// class BinaryTree {
+//   constructor(value){
+//     let init = new Node(value);
+//     this.root = init;
+//     this.cnt = 0;
+//   }
+
+//   length() {
+//     return this.cnt;
+//   }
+
+//   insert(value) {
+//     let newNode = new Node(value);
+//     let currNode = this.root;
+
+//     while(currNode) {
+//       if(value === currNode.value) {
+//         // 데이터 중복은 있지 않다.
+//         return ;
+//       }
+
+//       if(value < currNode.value){
+//         // 들어온 데이터가 작으면 왼쪽에
+//         // 비어있으면 데이터를 넣고 비어있지 않으면 while반복
+//         if(!currNode.child[0]){
+//           currNode.child[0] = newNode;
+//           return ;
+//         }
+//         currNode = currNode.child[0];
+//       } else {
+//         if(!currNode.child[1]){
+//           currNode.child[1] = newNode;
+//           return ;
+//         }
+//         currNode = currNode.child[1];
+//       }
+//     }
+
+//     this.cnt += 1;
+//   }
+
+//   DFS() {
+//     // 깊이 우선 탐색, 스택 이용
+//     let visitPath = [];
+//     let stack = [this.root];
+
+//     while(stack.length){
+//       let current = stack.pop();
+//       let left = current.child[0], right = current.child[1];
+//       if(left)
+//         stack.push(left);
+//       if(right)
+//         stack.push(right);
+
+//       visitPath.push(current.value);
+//     }
+
+//     return visitPath;
+//   }
+
+
+//   BFS() {
+//     // 너비 우선 탐색, 큐 이용
+//     let visitPath = [];
+//     let que = [this.root];
+
+//     while(que.length){
+//       let current = que.shift();
+//       let left = current.child[0], right = current.child[1];
+//       if(left)
+//         que.push(left);
+//       if(right)
+//         que.push(right);
+
+//       visitPath.push(current.value);
+//     }
+
+//     return visitPath;
+//   }
+// }
+
+// let t = new BinaryTree(5);
+// t.insert(3);
+// t.insert(8);
+// t.insert(1);
+// t.insert(4);
+// t.insert(6);
+// t.insert(9);
+
+// console.log(t.DFS());
+// console.log(t.BFS());
+
+
+
+// 실전 코딩테스트 풀이
+// 카카오 18년도
+// https://programmers.co.kr/learn/courses/30/lessons/17681?language=javascript
+// 주제 : 2진법, 진법 연산, replace, or 연산
+// let n = 5;
+// let arr1 = [9, 20, 28, 18, 11];
+// let arr2 = [30, 1, 21, 17, 28];
+
+// function solution(n, arr1, arr2) {
+//     let result = []
+//     const zip = (a, b) => a.map((value, index)=>[value, b[index]]);
+//     const fillSpace = (n, arr) => { return ' '.repeat(n - arr.length) + arr }
+//     for (let [i, j] of zip(arr1, arr2)) {
+//         result.push(fillSpace(n, (i | j).toString(2).replace(/1/g, '#').replace(/0/g, ' ')));
+//     }
+//     return result;
+// }
+
+// console.log(solution(n, arr1, arr2));
+
+// https://programmers.co.kr/learn/courses/30/lessons/17682
+// 1S*2T*3S
+//[1* 2 * 2, 2^3 * 2, 3]
+//[4, 16, 3] = 23
+
+// const testcase = [
+//   '1S2D*3T',
+//   '1D2S#10S',
+//   '1D2S0T'
+// ];
+
+// for (const c of testcase) {
+//   console.log(solution(c));
+// }
+
+// const dartResult = '1S2D3T'
+// let answer = [];
+// let result = 0;
+// let temp = 0; // 임시점수
+
+// function solution(dartResult) {
+//   let answer = [];
+//   let result = 0;
+//   let temp = 0; // 임시점수
+  
+//   for (let i = 0; i < dartResult.length; i++) {
+//       // console.log(dartResult[i]);
+//       if (dartResult[i] >= 0 && dartResult[i] <=9 ) {
+//           if (dartResult[i] == 1 && dartResult[i+1] == 0) {
+//               temp = 10;
+//               i++;
+//           } else {
+//               temp = parseInt(dartResult[i]);
+//           }
+//       } else if (dartResult[i] == 'S'){
+//           answer.push(temp);
+//       } else if (dartResult[i] == 'D'){
+//           // answer.push(Math.pow(temp, 2));
+//           answer.push(temp**2);
+//       } else if (dartResult[i] == 'T'){
+//           // answer.push(Math.pow(temp, 3));
+//           answer.push(temp**3);
+//       } else if (dartResult[i] == '*'){
+//           answer[answer.length-1] *= 2;
+//           answer[answer.length-2] *= 2;
+//       } else if (dartResult[i] == '#'){
+//           answer[answer.length-1] *= -1;
+//       }
+//   }
+//   for (let i = 0; i < answer.length; i++) {
+//       result += answer[i];
+//   }
+//   return result;
+// }
+
+// https://tech.kakao.com/2017/09/27/kakao-blind-recruitment-round-1/
+// 캐시문제
+// 키워드 : LRU 알고리즘, 페이지 교체 알고리즘
+// 3	["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]	50
+// 3	["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]	21
+// 2	["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]	60
+
+const testcase = [
+  [3, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]],
+  [3, ["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]],
+  [2, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]]
+];
+
+for (const [cacheSize, cities] of testcase) {
+  console.log(solution(cacheSize, cities));
 }
-let input = [199, 22, 33, 12, 32, 64, 72, 222, 233];
-console.log(quickSort(input));
+
+// 50, 21, 60
+
+function solution(cacheSize, cities) {
+  let time = 0;
+  let cache = [];
+  for (let i = 0; i < cities.length; i++) {
+      let city = cities[i].toLowerCase();
+      let index = cache.indexOf(city);
+      if (index !== -1) {
+          // hit(캐시에 존재함)
+          cache.splice(index, 1);
+          cache.push(city);
+          time += 1;
+      } else {
+          // miss(캐시에 존재하지 않음)
+          time += 5;
+          cache.push(city);
+          if (cacheSize < cache.length) {
+              cache.shift();
+          }
+      }
+  }
+  return time;
+}
