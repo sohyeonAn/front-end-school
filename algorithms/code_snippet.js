e322f05c2f7be929f15561edf59cb07c647ab69c
sohyeonAn <ansh4393@gmail.com>
2021-12-21 17:07:08 +0900

    12월 21일 수업 - 알고리즘

--- algorithms/code_snippet.js
+++ algorithms/code_snippet.js
@@ -516,7 +516,7 @@
 //   let answer = [];
 //   let result = 0;
 //   let temp = 0; // 임시점수
-  
+
 //   for (let i = 0; i < dartResult.length; i++) {
 //       // console.log(dartResult[i]);
 //       if (dartResult[i] >= 0 && dartResult[i] <=9 ) {
@@ -554,37 +554,225 @@
 // 3	["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]	21
 // 2	["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]	60
 
-const testcase = [
-  [3, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]],
-  [3, ["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]],
-  [2, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]]
-];
+// const testcase = [
+//   [3, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]],
+//   [3, ["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]],
+//   [2, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]]
+// ];
 
-for (const [cacheSize, cities] of testcase) {
-  console.log(solution(cacheSize, cities));
-}
+// for (const [cacheSize, cities] of testcase) {
+//   console.log(solution(cacheSize, cities));
+// }
 
-// 50, 21, 60
+// // 50, 21, 60
 
-function solution(cacheSize, cities) {
-  let time = 0;
-  let cache = [];
-  for (let i = 0; i < cities.length; i++) {
-      let city = cities[i].toLowerCase();
-      let index = cache.indexOf(city);
-      if (index !== -1) {
-          // hit(캐시에 존재함)
-          cache.splice(index, 1);
-          cache.push(city);
-          time += 1;
+// function solution(cacheSize, cities) {
+//   let time = 0;
+//   let cache = [];
+//   for (let i = 0; i < cities.length; i++) {
+//       let city = cities[i].toLowerCase();
+//       let index = cache.indexOf(city);
+//       if (index !== -1) {
+//           // hit(캐시에 존재함)
+//           cache.splice(index, 1);
+//           cache.push(city);
+//           time += 1;
+//       } else {
+//           // miss(캐시에 존재하지 않음)
+//           time += 5;
+//           cache.push(city);
+//           if (cacheSize < cache.length) {
+//               cache.shift();
+//           }
+//       }
+//   }
+//   return time;
+// }
+
+
+// 2. 19년도
+// 링크 : https://tech.kakao.com/2018/09/21/kakao-blind-recruitment-for2019-round-1/
+// 1번 오픈채팅방 문제
+// let record = [
+//   "Enter uid1234 Muzi", 
+//   "Enter uid4567 Prodo",
+//   "Leave uid1234",
+//   "Enter uid1234 Prodo",
+//   "Change uid4567 Ryan"
+// ]
+
+// function solution(record){
+//   let result = [];
+//   let answer = [];
+//   let 유저정보 = {};
+
+//   for (const iterator of record) {
+//       const [상태, 아이디, 닉네임] = iterator.split(' ');
+//       if (상태 === 'Enter'){
+//           유저정보[아이디] = 닉네임;
+//           result.push([아이디, '님이 들어왔습니다.']);
+//       } else if (상태 === 'Leave'){
+//           result.push([아이디, '님이 나갔습니다.']);
+//       } else if (상태 === 'Change'){
+//           유저정보[아이디] = 닉네임;
+//       }
+//   }
+
+//   // 지금 우리 데이터
+//   // [[uid1234, 님이 들어왔습니다.], [uid4567, 님이 들어왔습니다.]]
+
+//   for (const [아이디, 메시지] of result) {
+//       answer.push(유저정보[아이디] + 메시지);
+//   }
+
+//   return answer;
+// }
+
+// solution(record);
+
+// 2번 실패율
+// https://tech.kakao.com/2018/09/21/kakao-blind-recruitment-for2019-round-1/
+
+// 실패율 === 아직 클리어 못한 플레이어의 수 / 도달한 플레이어 수
+// 전체 스테이지의 개수 N
+// 스테이지의 번호가 담긴 배열 stages가 매개변수
+
+// 실패율이 높은 스테이지부터 내림차순으로 스테이지의 번호가 담겨있는 배열을 return 하도록 solution 함수
+// 만약 실패율이 같은 스테이지가 있다면 작은 번호의 스테이지가 먼저 오도록 하면 된다. (오름차순)
+
+// N	stages                  	result
+// 5	[2, 1, 2, 6, 2, 4, 3, 3]	[3, 4, 2, 1, 5]
+// 4	[4, 4, 4, 4, 4]	            [4, 1, 2, 3]
+
+// 스테이지에 도달한 사람의 수
+// 1stage === 1
+// 2stage === 3
+// 3stage === 2
+// 4stage === 1
+// 5stage === 0
+
+
+// 실패율
+// 1stage === 1/8
+// 2stage === 3/7 === 3/(8-1)
+// 3stage === 2/4 === 2/(7-3)
+// 4stage === 1/2 === 1/(4-2)
+// 5stage === 0/1 === 0/(2-1)
+
+
+// [2, 1, 2, 6, 2, 4, 3, 3].filter((user) => user === 3);
+// (2) [3, 3]
+
+// function solution(스테이지수, stages) {
+//   let 실패율 = [];
+//   let 유저수 = stages.length;
+
+//   for (let i = 1; i <= 스테이지수; i++) {
+//       let 도달한사람수 = stages.filter((user) => user === i).length;
+//       let 확률 = 도달한사람수/유저수;
+//       유저수 -= 도달한사람수;
+//       실패율.push({stage : i, 확률 : 확률});
+//   }
+
+//   // sort의 내림차순
+//   // b - a
+//   // sort의 오름차순
+//   // a - b
+//   실패율.sort((a, b) => {
+//       if (a.확률 === b.확률){
+//           return a.stage - b.stage;
+//       } else {
+//           return b.확률 - a.확률;
+//       }
+//   });
+
+//   return 실패율.map(object => object.stage);
+// }
+
+// solution(5, [2, 1, 2, 6, 2, 4, 3, 3]);
+
+// 3. 20년도
+// 링크 : https://tech.kakao.com/2019/10/02/kakao-blind-recruitment-2020-round1/
+// aabbaccc -> a, a, b, b, a, c, c, c -> 2a2ba3c
+// aabbaccc -> aa, bb, ac, cc -> aabbaccc
+// aabbaccc -> aab, bac, cc -> aabbaccc
+// aabbaccc -> aabb, accc -> aabbaccc
+
+// 'aabbaccc'.match(/[a-z]{1}/g);
+// 'aabbaccc'.match(/[a-z]{2}/g);
+
+function solution(s) {
+  var answer = s.length;
+  let len = s.length;
+
+  if (len === 1) return 1;
+
+  for (let i = 1; i <= len / 2 + 1; i++) { //자르는 크기
+    const re = new RegExp(`[a-z]{${i}}`, 'g');
+    console.log('re', re)
+    let 잘린문자열 = s.match(re);
+    console.log('잘린문자열', 잘린문자열)
+    압축문자열 = ''
+    let count = 1 //2a2b3c 여기서 앞에 나오는 숫자
+    for (j = 0; j < 잘린문자열.length; j++) {
+      if (잘린문자열[j] === 잘린문자열[j + 1]) {
+        //aa -> 2a // 뒤에 값을 봐서 같은지!
+        count += 1;
       } else {
-          // miss(캐시에 존재하지 않음)
-          time += 5;
-          cache.push(city);
-          if (cacheSize < cache.length) {
-              cache.shift();
-          }
+        if (count === 1) {
+          압축문자열 += `${잘린문자열[j]}`;
+        } else if (count > 1) {
+          // 왜 1보다 크냐면 aa가 1a1a가 아니기 때문!
+          압축문자열 += `${count}${잘린문자열[j]}`;
+          // 앞 문자열과 뒤 문자열이 다른 경우는
+          count = 1;
+        }
+        console.log('압축문자열', 압축문자열);
       }
+
+    }
+    if (len % i !== 0) {
+      압축문자열 += s.slice(-len % i)
+    }
+    // console.log(압축문자열);
+    answer = Math.min(answer, 압축문자열.length)
   }
-  return time;
+  return answer;
 }
+
+solution("aabbaccc")
+// solution("aabbaabbaccc") // accc를 추가하지 못하는 문제 발생
+
+
+// 번외
+// 입력 예시: aaabbcccccca
+// 출력 예시: a3b2c6a1
+
+// let s = 'aaabbcccccca';
+// let 압축 = '';
+// let count = 1
+
+// for (let i = 0; i < s.length; i++) {
+//   if (s[i] === s[i + 1]){
+//               count += 1
+//   } else if (count >= 1) {
+//       압축 += `${s[i]}${count}`;
+//       count = 1;
+//   }
+// }
+
+// console.log(압축);
+
+
+
+// why?
+// for (const index in s) {
+//     console.log(s[index] === s[index+1])
+//     console.log(s[index], s[index+1])
+//     // if (s[index] === s[index + 1]){
+//     //     count += 1
+//     // } else if (count >= 1) {
+//     //     압축 += `${s[index]}${count}`;
+//     //     count = 1;
+//     // }
+// }
